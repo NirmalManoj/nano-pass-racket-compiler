e3fc0814d2f3d93c5837d599acfc6c2ed43d85cf
Nirmal Manoj <nimmumanoj@gmail.com>
2022-04-27 01:39:32 +0530

    more tests

---
+++ tests/functions_test_7.rkt
@@ -0,0 +1,10 @@
+ (define
+ (id [a : (Vector Integer)] [b : Integer]
+  [c : Integer] [d : (Vector Integer Integer)] [e : Integer]
+  [f : Integer] [g : Integer] [h : Integer]) : Integer
+ (+ (+ (vector-ref a 0) (+ b (+ c (+ (vector-ref d 2) (+ e (+ f (+ g g)))))))
+    7)
+ )
+
+;;;  (id (vector 1) 2 3 4 5 6 7 (vector 42 (vector 42 7)))
+;;;  (id (vector 1) 2 3 (vector 40 4) 5 6 7 14)
